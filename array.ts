315fd6a0862d10c5a9734fbfd9fa5548166ce420
Tahira Ahmed <tahira.ahmed92@hotmail.com>
2016-07-15 19:43:22 +0500

    update practice

--- array.ts
+++ array.ts
@@ -1,3 +1,17 @@
-var arrayOfNumbers: number[] = [1, 2, 3];
+let arrayOfNumbers: number[] = [1, 2, 3]; // this array is strongly typed which is of type number
 arrayOfNumbers = [3, 4, 5];
 arrayOfNumbers = ["one", "two", "three"];
+
+let array1 : number[] = [5, 6, 7]; //correct syntax
+console.log(array1[1]); // results 6
+
+let array2:Array<number> = [1, 2, 3]; //alternative correct syntax
+let array3 : number[] = []; //correct syntax to define an empty array
+
+let array4: number[] = new number[2]; //error
+
+let array5: number[] = [];
+array5.push(1234); //dynamically adding
+console.log(array5);
+console.log(array5[0]);
+
